44abb428daf09098c50b67673ccc5a9b016914d2
Andrei Arekhva <a.arekhva@gmail.com>
2023-12-01 08:47:28 +0300

    Add xmlns definition

---
+++ src/XRSharp.CommunityToolkit.Networked/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://xrsharp.io/xr/toolkit", "XRSharp.CommunityToolkit.Networked")]
+[assembly: XmlnsPrefix("http://xrsharp.io/xr/toolkit", "xrt")]
