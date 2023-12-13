47eb64880d03ff7b0140d482c8a1c3267142ae24
Andrei Arekhva <a.arekhva@gmail.com>
2023-12-13 17:08:40 +0300

    Networked: fix hand tracking

--- src/XRSharp.CommunityToolkit.Networked/Resources/networked-hand-tracking.js
+++ src/XRSharp.CommunityToolkit.Networked/Resources/networked-hand-tracking.js
@@ -8,15 +8,12 @@
     init: function () {
         var el = this.el;
         this.trackingControls = el.components['hand-tracking-controls'];
-        this.trackingControls.initDotsModel = this.initDotsModel();
 
         this.onExitVR = AFRAME.utils.bind(this.onExitVR, this);
         el.sceneEl.addEventListener('exit-vr', this.onExitVR);
     },
 
-    initDotsModel: function () {
-        this.trackingControls.initDotsModel();
-
+    tick: function () {
         var jointEls = this.trackingControls.jointEls;
 
         for (var i = 0; i < jointEls.length; i++) {
